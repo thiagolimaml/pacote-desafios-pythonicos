39cc2d2ac3ae22e60207fd2909546bb4b854fad4
Thiago Lima <thiagolimaml@gmail.com>
2020-03-30 16:00:29 -0300

    ex_12 == ok

--- 12_linear_merge.py
+++ 12_linear_merge.py
@@ -11,7 +11,12 @@ A sua solução deve rodar em tempo linear, ou seja, deve fazer uma
 
 def linear_merge(list1, list2):
     # +++ SUA SOLUÇÃO +++
-    return
+    lista = list()
+    list1.sort()
+    list2.sort()
+    lista = list1 + list2
+    lista.sort()
+    return lista
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
