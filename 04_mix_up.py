1b4f7fe9ac8e08e0f938ca745c230d0619cfb888
Thiago Lima <thiagolimaml@gmail.com>
2020-03-30 11:34:14 -0300

    ex_04 == ok

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -14,7 +14,9 @@ Assuma que a e b tem tamanho 2 ou maior.
 
 def mix_up(a, b):
     # +++ SUA SOLUÇÃO +++
-    return
+    c = a.replace(a[:2], b[:2])
+    d = b.replace(b[:2], a[:2])
+    return f'{c} {d}'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
